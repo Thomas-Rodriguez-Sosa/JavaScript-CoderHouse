5a8992e0ea23e66d88a1d370172df01a80f5e094
Thomas Rodriguez <rodriguezsosathomas@gmail.com>
2024-02-07 15:47:54 -0300

    Xtulator Version 2.5

--- JS/calcular.js
+++ JS/calcular.js
@@ -1,55 +1,71 @@
-class Alumno {
-    constructor (nombre, edad, nacionalidad, dni, promedio) {
-        this.nombre = `${nombre}`;
-        this.edad = edad;
-        this.nacionalidad = `${nacionalidad}`;
-        this.dni = dni;
-        this.promedio = promedio;
-    }
-}
-// Alumnos ya registrados //
-const alumno1 = new Alumno("Thomas Rodriguez", 18, "Argentino", 44971946, 8)
-const alumno2 = new Alumno("Lisandro Correa", 17, "Argentino", 45594408, 9,5) 
-const alumno3 = new Alumno("Tomas Galvan", 18, "Argentino", 44985832, 4)
-// Array de Alumnos registrados ///
-const listaAlumnos = [alumno1, alumno2, alumno3]
+// Query Selectors //
 const submitCalc = document.querySelector("#submit")
+const clear = document.querySelector("#clear")
 const input1 = document.querySelector("#input-1")
 const input2 = document.querySelector("#input-2")
 const input3 = document.querySelector("#input-3")
 const listaRegis = document.querySelector("#listaRegis")
 const listaAlumnosR = document.querySelector("#listaAlumnosR ul")
 const botonLista = document.querySelector("#botonLista")
-localStorage.setItem("listaAlumnos", JSON.stringify(listaAlumnos))
-const listaAlumnosJSON = JSON.parse(localStorage.getItem("listaAlumnos"));
+const tablaResul = document.querySelector("#tablaResul")
+let listaAlumnos;
 let resultadoInput = 0;
+// Fetch //
+fetch("/js/calcular.json")
+    .then(response => response.json())
+    .then(data => {
+        mostrarLista(data)
+        console.log(data)
+    })
+
+
+
+
 
 submitCalc.addEventListener("click", calcularEmpieza)
+clear.addEventListener("click", limpiarEmpieza)
 botonLista.addEventListener("click", mostrarLista)
 
 function calcularEmpieza(e) {
     e.preventDefault();
     if (input1.value === "" || input2.value === "" || input3 === "") {
-        alert ("Debes ingresar numeros validos para continuar")
+        Toastify({
+            text: "Numeros invalidos",
+            duration: 2000,
+            destination: "#",
+            newWindow: false,
+            close: true,
+            gravity: "bottom", 
+            position: "right", 
+            stopOnFocus: false, 
+            style: {
+              background: "red",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
     } else {
         resultadoInput = (parseFloat(input1.value) + parseFloat(input2.value) + parseFloat(input3.value)) / 3;
         
-        alert (`Tu promedio es de: ${resultadoInput}`)
+        tablaResul.innerHTML += `<th>Promedio Final: ${resultadoInput}</th>`
     }
 }
-function mostrarLista() {
-    botonLista.innerText = "Ocultar lista de estudiantes recientes"
-    if (listaRegis.style.display === "none" || listaRegis.style.display === "") {
+
+// Functions //
+function limpiarEmpieza () {
+    tablaResul.remove()
+}
+
+function mostrarLista(alumnos) {
+    botonLista.innerText = "Ocultar estudiantes recientes"
+    if (listaRegis.style.display === "none" || listaRegis.style.display === null || listaRegis.style.display === "") {
         listaRegis.style.display = "block";
-        listaRegis.innerHTML = "";
-        for (Alumno of listaAlumnos) {
-            listaRegis.innerHTML += `<li class="lista-Item">${Alumno.nombre}</li>`
-        }
-        listaAlumnosR.append(listaRegis)
+        alumnos.forEach(alumno => {
+            const li = document.createElement("li")
+            li.textContent = `${alumno.nombre} - ${alumno.promedio}`
+            listaRegis.appendChild(li)
+        }); 
     } else {
         listaRegis.style.display = "none";
         botonLista.innerText = "Mostrar estudiantes recientes"
     }
 }
-
-
