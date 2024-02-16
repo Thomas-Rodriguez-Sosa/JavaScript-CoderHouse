0e5e44fdb327d80eb86edc8ec06b6293ca1047a2
Thomas Rodriguez <rodriguezsosathomas@gmail.com>
2024-02-16 20:31:51 -0300

    Xtulator Version 3.0

--- JS/calcular.js
+++ JS/calcular.js
@@ -1,6 +1,7 @@
 // Query Selectors //
 const submitCalc = document.querySelector("#submit")
 const clear = document.querySelector("#clear")
+const inputNombre = document.querySelector("#input-nombre")
 const input1 = document.querySelector("#input-1")
 const input2 = document.querySelector("#input-2")
 const input3 = document.querySelector("#input-3")
@@ -10,25 +11,14 @@ const botonLista = document.querySelector("#botonLista")
 const tablaResul = document.querySelector("#tablaResul")
 let listaAlumnos;
 let resultadoInput = 0;
-// Fetch //
-fetch("/js/calcular.json")
-    .then(response => response.json())
-    .then(data => {
-        mostrarLista(data)
-        console.log(data)
-    })
-
-
-
-
 
 submitCalc.addEventListener("click", calcularEmpieza)
 clear.addEventListener("click", limpiarEmpieza)
-botonLista.addEventListener("click", mostrarLista)
+botonLista.addEventListener("click", () => mostrarLista());
 
 function calcularEmpieza(e) {
     e.preventDefault();
-    if (input1.value === "" || input2.value === "" || input3 === "") {
+    if (input1.value === "" || input2.value === "" || input3 === "" || inputNombre === "") {
         Toastify({
             text: "Numeros invalidos",
             duration: 2000,
@@ -41,31 +31,38 @@ function calcularEmpieza(e) {
             style: {
               background: "red",
             },
-            onClick: function(){} // Callback after click
+            onClick: function(){}
           }).showToast();
     } else {
+        tablaResul.innerHTML = "";
         resultadoInput = (parseFloat(input1.value) + parseFloat(input2.value) + parseFloat(input3.value)) / 3;
-        
+        // Usando Local Storage puedo guardar el nombre y promedio del nuevo alumno para asi poder mostrarlos en la lista de "Alumnos Recientes"
+        let nuevoAlumno = {nombre: inputNombre.value, promedio: resultadoInput}
+        let alumnos = JSON.parse(localStorage.getItem("alumnos"))
+        alumnos.push(nuevoAlumno)
+        localStorage.setItem("alumnos", JSON.stringify(alumnos))
         tablaResul.innerHTML += `<th>Promedio Final: ${resultadoInput}</th>`
     }
 }
-
 // Functions //
 function limpiarEmpieza () {
     tablaResul.remove()
 }
 
-function mostrarLista(alumnos) {
+// Cada vez que clickea el boton "Mostrar lista" esta misma lista se actualiza con el ultimo alumno que haya sido agregado en Local Storage. //
+function mostrarLista() {
+    listaRegis.innerHTML = "";
     botonLista.innerText = "Ocultar estudiantes recientes"
+    alumnos = JSON.parse(localStorage.getItem("alumnos"))
     if (listaRegis.style.display === "none" || listaRegis.style.display === null || listaRegis.style.display === "") {
-        listaRegis.style.display = "block";
+        listaRegis.style.display = "block"
         alumnos.forEach(alumno => {
             const li = document.createElement("li")
             li.textContent = `${alumno.nombre} - ${alumno.promedio}`
             listaRegis.appendChild(li)
-        }); 
+        });    
     } else {
-        listaRegis.style.display = "none";
+        listaRegis.style.display = "none"
         botonLista.innerText = "Mostrar estudiantes recientes"
     }
 }
--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,9 @@
-fetch("/js/index.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+const alumnos = [
+    {nombre: "Thomas Rodriguez", edad: 20, nacionalidad: "Argentina", dni: 44971946, promedio: 7},
+    {nombre: "Lisandro Correa", edad: 17, nacionalidad: "Argentina", dni: 23234322, promedio: 3},
+    {nombre: "Tomas Galvan", edad: 19, nacionalidad: "Argentina", dni: 54566334, promedio: 8},
+    {nombre: "Lucio Raffin", edad: 18, nacionalidad: "Argentina", dni: 44456432, promedio: 5}
+]
+// Este Array guardado en Local Storage tiene su uso en calcular.js //
+localStorage.setItem("alumnos", JSON.stringify(alumnos))
+
