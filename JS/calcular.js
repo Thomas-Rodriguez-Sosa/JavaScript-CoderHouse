e258973b50a937feb2fe59b0d113474953d7bfd4
Thomas Rodriguez <rodriguezsosathomas@gmail.com>
2024-02-25 18:31:39 -0300

    Xtulator V. 3.0

--- JS/calcular.js
+++ JS/calcular.js
@@ -9,6 +9,10 @@ const listaRegis = document.querySelector("#listaRegis")
 const listaAlumnosR = document.querySelector("#listaAlumnosR ul")
 const botonLista = document.querySelector("#botonLista")
 const tablaResul = document.querySelector("#tablaResul")
+let signUpBtn = document.querySelector("#signUpBtn")
+let signInBtn = document.querySelector("#signInBtn")
+let nombreInput = document.querySelector("#nameInput")
+let formTitle = document.querySelector("#formTitle")
 document.querySelector("#buttonRegis").addEventListener("click", function(){
     document.querySelector(".popup-Form").classList.add("active")
     document.querySelector(".footerIcon").classList.add("invisibleIcon")
@@ -17,13 +21,49 @@ document.querySelector(".popup-Form .btn-cerrar").addEventListener("click", func
     document.querySelector(".popup-Form").classList.remove("active")
     document.querySelector(".footerIcon").classList.remove("invisibleIcon")
 });
-let listaAlumnos;
-let resultadoInput = 0;
+document.querySelector("#botonBorrar").addEventListener("click", function(e){
+    e.stopPropagation();
+    localStorage.clear();
+    return localStorage.setItem("alumnos", JSON.stringify(alumnos = []))
+})
+
+signInBtn.addEventListener("click", function(){
+    if (signInBtn.innerHTML == "Iniciar Sesion") {
+        nombreInput.style.maxHeight = "0";
+        formTitle.innerHTML = "Iniciar Sesion";
+        signUpBtn.innerHTML = "Iniciar Sesion";
+        signInBtn.innerHTML = "¿No tiene una cuenta? Registrese aqui.";
+    } else {
+        nombreInput.style.maxHeight = "80px";
+        formTitle.innerHTML = "Registrarse";
+        signUpBtn.innerHTML = "Registrarse";
+        signInBtn.innerHTML = "Iniciar Sesion";
+    }
+});
 
 submitCalc.addEventListener("click", calcularEmpieza)
 clear.addEventListener("click", limpiarEmpieza)
 botonLista.addEventListener("click", () => mostrarLista());
+listaAlumnosR.addEventListener("click", borrarUltimo)
+let resultadoInput = 0;
 
+// Functions //
+function listener() {
+    document.addEventListener("DOMContentLoaded", () => {
+        alumnos = JSON.parse(localStorage.getItem("alumnos"));
+    })
+}
+function actualizarLista() {
+    listaRegis.innerHTML = "";
+    alumnos = JSON.parse(localStorage.getItem("alumnos")) || alumnos;
+    alumnos.forEach(alumno => {
+        const li = document.createElement("li")
+        li.innerHTML = `${alumno.nombre} - ${alumno.promedio} <span alumnoid="${alumno.id}" class="btn-Borrar">X</span>`
+        listaRegis.appendChild(li)
+    });    
+    listener();
+    localStorage.setItem("alumnos", JSON.stringify(alumnos));
+}
 function calcularEmpieza(e) {
     e.preventDefault();
     if (input1.value === "" || input2.value === "" || input3 === "" || inputNombre === "") {
@@ -45,32 +85,37 @@ function calcularEmpieza(e) {
         tablaResul.innerHTML = "";
         resultadoInput = (parseFloat(input1.value) + parseFloat(input2.value) + parseFloat(input3.value)) / 3;
         // Usando Local Storage puedo guardar el nombre y promedio del nuevo alumno para asi poder mostrarlos en la lista de "Alumnos Recientes"
-        let nuevoAlumno = {nombre: inputNombre.value, promedio: resultadoInput}
+        let nuevoAlumno = {nombre: inputNombre.value, promedio: resultadoInput, id:Date.now()}
         let alumnos = JSON.parse(localStorage.getItem("alumnos"))
-        alumnos.push(nuevoAlumno)
-        localStorage.setItem("alumnos", JSON.stringify(alumnos))
+        alumnos = [...alumnos, nuevoAlumno]
+        actualizarLista();
+        localStorage.setItem("alumnos", JSON.stringify(alumnos));
+        nuevoAlumno = "";
+        inputNombre.value = '';
+        input1.value = '';
+        input2.value = '';
+        input3.value = '';
         tablaResul.innerHTML += `<th>Promedio Final: ${resultadoInput}</th>`
     }
 }
-// Functions //
 function limpiarEmpieza () {
     tablaResul.remove()
 }
-
+function borrarUltimo(e) {
+    if (e.target.classList.contains("btn-Borrar")) {
+        const borrarAtt = parseInt(e.target.getAttribute("alumnoid"))
+        alumnos = alumnos.filter(alumno => alumno.id !== borrarAtt)
+        localStorage.setItem("alumnos", JSON.stringify(alumnos))
+    }
+}
 // Cada vez que clickea el boton "Mostrar lista" esta misma lista se actualiza con el ultimo alumno que haya sido agregado en Local Storage. //
 function mostrarLista() {
-    listaRegis.innerHTML = "";
-    botonLista.innerText = "Ocultar estudiantes recientes"
-    alumnos = JSON.parse(localStorage.getItem("alumnos"))
     if (listaRegis.style.display === "none" || listaRegis.style.display === null || listaRegis.style.display === "") {
-        listaRegis.style.display = "block"
-        alumnos.forEach(alumno => {
-            const li = document.createElement("li")
-            li.textContent = `${alumno.nombre} - ${alumno.promedio}`
-            listaRegis.appendChild(li)
-        });    
+        listaRegis.style.display = "block"    
+        botonLista.innerText = "Ocultar estudiantes recientes"
     } else {
         listaRegis.style.display = "none"
         botonLista.innerText = "Mostrar estudiantes recientes"
     }
+    actualizarLista();
 }
--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,10 @@
 const alumnos = [
-    {nombre: "Thomas Rodriguez", edad: 20, nacionalidad: "Argentina", dni: 44971946, promedio: 7},
-    {nombre: "Lisandro Correa", edad: 17, nacionalidad: "Argentina", dni: 23234322, promedio: 3},
-    {nombre: "Tomas Galvan", edad: 19, nacionalidad: "Argentina", dni: 54566334, promedio: 8},
-    {nombre: "Lucio Raffin", edad: 18, nacionalidad: "Argentina", dni: 44456432, promedio: 5}
+    {nombre: "Thomas Rodriguez", promedio: 7, id:1131354323},
+    {nombre: "Lisandro Correa", promedio: 3, id:1233354323},
+    {nombre: "Tomas Galvan", promedio: 8, id:1231354423},
+    {nombre: "Lucio Raffin", promedio: 5, id:1231354323}
 ]
+
 // Este Array guardado en Local Storage tiene su uso en calcular.js //
 localStorage.setItem("alumnos", JSON.stringify(alumnos))
 
@@ -15,4 +16,21 @@ document.querySelector(".popup-Form .btn-cerrar").addEventListener("click", func
     document.querySelector(".popup-Form").classList.remove("active")
     document.querySelector(".footerIcon").classList.remove("invisibleIcon")
 });
-    
+let signUpBtn = document.querySelector("#signUpBtn")
+let signInBtn = document.querySelector("#signInBtn")
+let nombreInput = document.querySelector("#nameInput")
+let formTitle = document.querySelector("#formTitle")
+
+signInBtn.addEventListener("click", function(){
+    if (signInBtn.innerHTML == "Iniciar Sesion") {
+        nombreInput.style.maxHeight = "0";
+        formTitle.innerHTML = "Iniciar Sesion";
+        signUpBtn.innerHTML = "Iniciar Sesion";
+        signInBtn.innerHTML = "¿No tiene una cuenta? Registrese aqui.";
+    } else {
+        nombreInput.style.maxHeight = "80px";
+        formTitle.innerHTML = "Registrarse";
+        signUpBtn.innerHTML = "Registrarse";
+        signInBtn.innerHTML = "Iniciar Sesion";
+    }
+});
